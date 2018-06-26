45e794eb76a5a704bc0ef5ac98c019deaf43d7e1
Robby Heenan <robbyheenan@Robbys-MBP.hsd1.ga.comcast.net>
2018-06-26 11:59:59 -0400

    tweaked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -38,7 +38,7 @@ $( document ).ready(function() {
     
     function displayGifs(){
         var animal = $(this).attr("data-name");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=NI3vrU8jSJh6fAgKh0lkQWWTgT0RE7gg&limit=10";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=NI3vrU8jSJh6fAgKh0lkQWWTgT0RE7gg&limit=10";
         console.log(queryURL); 
         $.ajax({
             url: queryURL,
